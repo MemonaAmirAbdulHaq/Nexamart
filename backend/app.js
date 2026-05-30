07a4be609a0f8cd685f255e0a4a4dc9c60de06bb
Memona Amir <157165172+MemonaAmirAbdulHaq@users.noreply.github.com>
2026-05-30 20:51:58 +0500

    Fix CORS origin URL by removing trailing slash

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 app.use(cors({
   origin: ['http://localhost:5173',
-    'https://nexamart-jade.vercel.app/'
+    'https://nexamart-jade.vercel.app'
   ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
