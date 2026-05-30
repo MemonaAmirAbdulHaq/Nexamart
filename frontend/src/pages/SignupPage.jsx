34408394f032e3bc6ceff954a4f01bb3e0148526
Memona Amir <157165172+MemonaAmirAbdulHaq@users.noreply.github.com>
2026-05-30 23:21:18 +0500

    Remove package.json and vercel.json configuration files

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,29 +1,4 @@
 
-// import React, { useEffect } from 'react';
-// import { useSelector } from 'react-redux';
-// import { useNavigate } from 'react-router-dom';
-// import Signup from "../components/Signup/Signup";
-
-// const SignupPage = () => {
-//   const navigate = useNavigate();
-//   const { isAuthenticated, loading } = useSelector((state) => state.user);
-
-//   useEffect(() => {
-//     if (isAuthenticated === true) {
-//       navigate("/");
-//     }
-//   }, [isAuthenticated, navigate]);
-
-//   return (
-//     <div className="w-full min-h-screen flex justify-center items-start p-4 bg-gray-50">
-//       <div className="w-full max-w-md bg-white shadow-md rounded-lg p-6">
-//         <Signup />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default SignupPage;
 import { useNavigate } from "react-router-dom";
 import SignUp from "../components/Signup/Signup.jsx";
 import { useSelector } from "react-redux";
